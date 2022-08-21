e84a4269324001d67dd5f7579f1d41141abc05ef
idris <idrisbahce26@gmail.com>
2022-08-21 22:34:11 +0300

    first listing of mine

---
+++ src/chapters/chapter02listings/ComputingArea.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class ComputingArea {
+	public static void main(String[] args) {
+		double area;
+		double radius;
+		
+		radius=15;
+		
+		area=radius*radius*3.14159;
+		
+		System.out.println("The area for circle of the radius " + radius 
+				+ " is " + area);
+		
+		
+	}
+
+}
