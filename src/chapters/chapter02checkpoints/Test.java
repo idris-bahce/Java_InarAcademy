8227d0f9da0134053af8fab62e1575021055161d
idris <idrisbahce26@gmail.com>
2022-08-21 23:03:39 +0300

    checkpoint2.1 is done!

---
+++ src/chapters/chapter02checkpoints/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+	 public static void main(String[] args) {
+	 double i = 50.0;
+	 double k = i + 50.0;
+	 double j = k + 1;
+	// I have corrected the failures. Now program runs perfectly.
+	System.out.println("j is " + j + " and k is " + k);
+	 }
+	
+
+}
+
