c2158a1eb27c87aa2744716e92077efa2b532ce1
idris <idrisbahce26@gmail.com>
2022-08-22 13:24:32 +0300

    here are the checkpoints 2.7, 2.8 and 2.9

---
+++ src/chapters/chapter02/chapter02checkpoints/checkpoint2.7/Tests.java
@@ -0,0 +1,10 @@
+
+public class Tests {
+ public static void main(String[] args) {
+	final int SIZE = 20;
+	System.out.println("Our number is " + SIZE);
+ 	//when we use constants we don't have to rewrite a variable.
+	//It is easy to read.
+	}
+ }
+
