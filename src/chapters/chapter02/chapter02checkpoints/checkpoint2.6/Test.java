40d9d6609273f50db1972cac4a6fb05744718b12
idris <idrisbahce26@gmail.com>
2022-08-22 12:51:48 +0300

    Checkpoint2.6 is done!

---
+++ src/chapters/chapter02/chapter02checkpoints/checkpoint2.6/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+ public static void main(String[] args) {
+	
+ 	int i = 2;
+ 	int j = 2;
+ 	int k = 2;
+
+ System.out.println(i + " " + j + " " + k);
+ // All wrongs are corrected. now it runs well!
+ }
+ }
+
