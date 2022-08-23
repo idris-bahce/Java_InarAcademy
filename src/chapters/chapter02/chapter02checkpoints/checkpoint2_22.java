3a31b3e8ef130ee2432a205f113641a1f0ccd93a
idris <idrisbahce26@gmail.com>
2022-08-23 22:20:54 +0300

    needed to be commited again

--- src/chapters/chapter02/chapter02checkpoints/checkpoint2_22.java
+++ src/chapters/chapter02/chapter02checkpoints/checkpoint2_22.java
@@ -1,5 +1,14 @@
 package chapters.chapter02.chapter02checkpoints;
 
 public class checkpoint2_22 {
+	public static void main(String[] args) {
+		System.out.println(4 / (3 * (r + 34)) - (9 * (a + b * c)) + ((3 + d * (2 + a)) / (a + b * d)));
+		
+		double exponential;
+		
+		exponential = (r + 2.5);
+		System.out.println(Math.pow(exponential, 2.5 + t));
+		System.out.println(5.5 * Math.pow(exponential, 2.5 + t));
+	}
 
 }
