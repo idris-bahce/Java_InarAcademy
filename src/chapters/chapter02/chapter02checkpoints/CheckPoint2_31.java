df4bffdd731a371c426118113a2e0d48f5d01dab
idris <idrisbahce26@gmail.com>
2022-08-24 15:05:20 +0300

    checkpoint 2.31

---
+++ src/chapters/chapter02/chapter02checkpoints/CheckPoint2_31.java
@@ -0,0 +1,10 @@
+package chapters.chapter02.chapter02checkpoints;
+
+public class CheckPoint2_31 {
+	public static void main(String[] args) {
+		double amount = 5;
+		System.out.println(amount/2);
+		System.out.println(5/2);
+	}
+
+}
