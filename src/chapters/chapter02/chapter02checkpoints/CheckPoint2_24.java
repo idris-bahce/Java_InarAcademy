07be9bcffe706ff2d1ca75b7b37432ed62383972
idris <idrisbahce26@gmail.com>
2022-08-24 13:49:33 +0300

    checkpoint2.24

---
+++ src/chapters/chapter02/chapter02checkpoints/CheckPoint2_24.java
@@ -0,0 +1,12 @@
+package chapters.chapter02.chapter02checkpoints;
+
+public class CheckPoint2_24 {
+	public static void main(String[] args) {
+		double a = 6.5;
+		a += a + 1;
+		System.out.println(a);
+		a = 6;
+		a /= 2;
+		System.out.println(a);
+	}
+}
