27e74eb5d19560450d1f8ae55cbb1a5787ac0aa6
idris <idrisbahce26@gmail.com>
2022-08-24 22:59:07 +0300

    A lot of things commited

---
+++ src/chapters/chapter02/chapter02checkpoints/CheckPoint2_33.java
@@ -0,0 +1,27 @@
+package chapters.chapter02.chapter02checkpoints;
+import java.util.Scanner;
+
+public class CheckPoint2_33 {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("Enter a decimal number: (e.g:11.56)");
+		double amount = input.nextDouble();
+		int remainingAmount = (int)(amount * 100);
+		int dolars = (remainingAmount /100);
+		System.out.println("Here is the amount of dolars: " + dolars);
+		
+		int quarters = (int)(remainingAmount % 100);
+		System.out.println("Here is the amount of quarters: " + quarters / 25);
+		
+		int dimes = quarters % 25;
+		System.out.println("Here is the amount of dimes: " + dimes / 10);
+		
+		int nickels = dimes % 10;
+		System.out.println("Here is amount of nickels: " + nickels / 5);
+		
+		int pennies = nickels % 5;
+		System.out.println("Here is the amount of pennies: " + pennies);
+	}
+
+}
