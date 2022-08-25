1dadf5eaae5a3006ccb2f11974f079076d15fc49
idris <idrisbahce26@gmail.com>
2022-08-25 11:49:32 +0300

    checkpoint3.02

---
+++ src/chapters/chapter03/chapter03checkpoints/CheckPoint3_02.java
@@ -0,0 +1,15 @@
+package chapters.chapter03.chapter03checkpoints;
+
+public class CheckPoint3_02 {
+	public static void main(String[] args) {
+		int x = 1;
+		
+		System.out.println(x>0);
+		System.out.println(x<0);
+		System.out.println(x!=0);
+		System.out.println(x>=0);
+		System.out.println(x!=1);
+		
+	}
+
+}
