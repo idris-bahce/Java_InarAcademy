201505e9c7bae656c2d3729e4071ccfa51f2999c
idris <idrisbahce26@gmail.com>
2022-08-25 12:33:04 +0300

    checkpoint3.04

---
+++ src/chapters/chapter03/chapter03checkpoints/CheckPoint3_04.java
@@ -0,0 +1,20 @@
+package chapters.chapter03.chapter03checkpoints;
+
+import java.util.Scanner;
+
+public class CheckPoint3_04 {
+	public static void main(String[] args) {
+		int x ;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter an integer for y: ");
+		int y = input.nextInt();
+		
+		if (y > 0) {
+		System.out.println(x = 1);
+		}
+		
+	}
+
+}
