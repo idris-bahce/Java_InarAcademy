4f99e7e5aea78ac8af16ccdaf17cd9c233b2e5d1
idris <idrisbahce26@gmail.com>
2022-08-28 17:42:04 +0300

    quizes:3.12-13

--- src/chapters/chapter03/chapter03listings/Quiz3_12.java
+++ src/chapters/chapter03/chapter03listings/Quiz3_12.java
@@ -1,5 +1,24 @@
 package chapters.chapter03.chapter03listings;
 
+import java.util.Scanner;
+
 public class Quiz3_12 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a three digit integer: ");
+		int x = in.nextInt();
+		
+		int digit100 = x /100;
+		int digit10 = (x / 10) % 10;
+		int digit1 = (x % 10);
+		
+		if(digit1 == digit100) {
+			System.out.println(x + " is a palindrome.");
+		}else {
+			System.out.println(x + " is not a apaindrome.");
+		}
+	}
+
+
 
 }
