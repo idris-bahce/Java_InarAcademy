959ea5a9d1945c6c5cd3e7ff7f3b2f6466392b5a
idris <idrisbahce26@gmail.com>
2022-08-29 11:25:26 +0300

    quiz3_22

--- src/chapters/chapter03/chapter03quiz/Quiz3_22.java
+++ src/chapters/chapter03/chapter03quiz/Quiz3_22.java
@@ -1,5 +1,19 @@
 package chapters.chapter03.chapter03quiz;
 
+import java.util.Scanner;
+
 public class Quiz3_22 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a point with two coordinates: ");
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		
+		if(Math.pow(x * x + y * y, 0.5)<=10) {
+			System.out.println("(" + x + ", " + y + ") is in the border of the circle.");
+		}else {
+			System.out.println("(" + x + ", " + y + ") is not in the border of the circle.");
+		}
+	}
 
 }
