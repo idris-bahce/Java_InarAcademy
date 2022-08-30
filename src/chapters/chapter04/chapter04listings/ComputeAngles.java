bfb88d61098d0917f5b8a96f01256f874ccbf2b1
idris <idrisbahce26@gmail.com>
2022-08-30 16:10:27 +0300

    first listing

--- src/chapters/chapter04/chapter04listings/ComputeAngles.java
+++ src/chapters/chapter04/chapter04listings/ComputeAngles.java
@@ -1,5 +1,27 @@
 package chapters.chapter04.chapter04listings;
 
+import java.util.Scanner;
+
 public class ComputeAngles {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter three points: ");
+		double x1 = in.nextDouble();
+		double y1 = in.nextDouble();
+		double x2 = in.nextDouble();
+		double y2 = in.nextDouble();
+		double x3 = in.nextDouble();
+		double y3 = in.nextDouble();
+		
+		double a = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+		double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+		double c = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+		
+		double A = Math.toDegrees(Math.acos((a * a - b * b - c * c) / (-2 * b * c)));
+		double B = Math.toDegrees(Math.acos((b * b - a * a - c * c) / (-2 * a * c)));
+		double C = Math.toDegrees(Math.acos((c * c - b * b - a * a) / (-2 * a * b)));
+		
+		System.out.println("The three angles are " + Math.round(A * 100) / 100.0 + " " +Math.round(B * 100) / 100.0 + " " + Math.round(C * 100) / 100.0);
+	}
 
 }
