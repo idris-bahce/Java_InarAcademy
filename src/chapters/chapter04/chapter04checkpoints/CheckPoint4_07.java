8f0c60284ac0cd511488821f57059b105bba951a
idris <idrisbahce26@gmail.com>
2022-08-31 21:50:05 +0300

    checkpoint4.07

---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_07.java
@@ -0,0 +1,15 @@
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_07 {
+	public static void main(String[] args) {
+		double x = Math.log(Math.exp(5.5));
+		System.out.println(x);
+		double y = Math.exp(Math.log(5.5));
+		System.out.println(y);
+		double z = Math.asin(Math.sin(Math.PI / 6));
+		System.out.println(z);
+		double w = Math.sin(Math.asin(Math.PI / 6));
+		System.out.println(w);
+	}
+
+}
