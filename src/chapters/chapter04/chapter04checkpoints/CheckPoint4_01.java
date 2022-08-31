9d6336a4330edfef0469eac45d82382b6ffd4a02
idris <idrisbahce26@gmail.com>
2022-08-31 18:09:13 +0300

    checkpoints

---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_01.java
@@ -0,0 +1,23 @@
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_01 {
+	public static void main(String[] args) {
+		double a = 4 ;
+		System.out.println(Math.sqrt(a));
+		double b = Math.PI * 2;
+		System.out.println(Math.sin(b));
+		double c = 2 * Math.PI;
+		System.out.println(Math.cos(c));
+		double d = 2;
+		System.out.println(Math.pow(d, 2));
+		double e =Math.E;
+		System.out.println(Math.log(e));
+		double f = 1;
+		System.out.println(Math.exp(f));
+		double g = 2;
+		System.out.println(Math.max(g, Math.min(3, 4)));
+		double h = -2.5;
+		System.out.println(Math.rint(h));
+	}
+
+}
