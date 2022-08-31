5e3f48c1fe9c090673ccd8205d04db6168688d77
idris <idrisbahce26@gmail.com>
2022-08-31 20:29:56 +0300

    Checkpoints4.2-3-4

---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_03.java
@@ -0,0 +1,10 @@
+
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_03 {
+	public static void main(String[] args) {
+		double radians = Math.toRadians(47);
+		System.out.println(radians);
+	}
+
+}
---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_04.java
@@ -0,0 +1,9 @@
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_04 {
+	public static void main(String[] args) {
+		double x = Math.toDegrees(Math.PI / 7);
+		System.out.println(x);
+	}
+
+}
