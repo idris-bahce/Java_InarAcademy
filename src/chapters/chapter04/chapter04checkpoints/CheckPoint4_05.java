684777dcb605d580466b468063f5bcdd4ba8c694
idris <idrisbahce26@gmail.com>
2022-08-31 21:15:35 +0300

    checkpoint4_05

---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_05.java
@@ -0,0 +1,16 @@
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_05 {
+	public static void main(String[] args) {
+		
+	int x = (int)(Math.random() * 22) + 30;
+	int y = (int)(Math.random() * 1000);
+	double z = (int)(Math.random() * 61) + 5.5;
+	
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	
+	}
+
+}
