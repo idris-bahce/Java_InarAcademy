f11420b9dd346cf9017f2ae08bae39e47c5fe9dd
idris <idrisbahce26@gmail.com>
2022-08-31 18:20:56 +0300

    checkpoint4_01

--- src/chapters/chapter04/chapter04checkpoints/CheckPoint4_01.java
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_01.java
@@ -18,6 +18,26 @@ public class CheckPoint4_01 {
 		System.out.println(Math.max(g, Math.min(3, 4)));
 		double h = -2.5;
 		System.out.println(Math.rint(h));
+		double i = -2.5;
+		System.out.println(Math.ceil(i));
+		double j = -2.5;
+		System.out.println(Math.floor(j));
+		float k = -2.5f;
+		System.out.println(Math.round(k));
+		double l = -2.5;
+		System.out.println(Math.round(l));
+		double m = 2.5;
+		System.out.println(Math.rint(m));
+		double n = 2.5;
+		System.out.println(Math.ceil(n));
+		double o = 2.5;
+		System.out.println(Math.floor(o));
+		float p = 2.5f;
+		System.out.println(Math.round(p));
+		double q = 2.5;
+		System.out.println(Math.round(q));
+		double r = Math.abs(-2.5);
+		System.out.println(r);
 	}
 
 }
