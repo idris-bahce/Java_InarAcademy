a75a7c7cc526de98513633114deeeaecc0e973c0
idris <idrisbahce26@gmail.com>
2022-09-01 21:54:52 +0300

    checkpoint4.12

---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_12.java
@@ -0,0 +1,23 @@
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_12 {
+	public static void main(String[] args) {
+		char c = 'A';
+		int i = (int)c;
+		System.out.println(i);
+		
+		float f = 1000.34f;
+		int a = (int)f;
+		System.out.println(a); 
+		
+		double d = 1000.34;
+		int b = (int)d;
+		System.out.println(b); 
+		
+		int k = 97;
+		char l = (char)k;
+		System.out.println(l);
+		
+	}
+
+}
