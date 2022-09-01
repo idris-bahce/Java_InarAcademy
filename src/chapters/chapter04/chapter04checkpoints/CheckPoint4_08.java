b57da748133c346e9c1ecb60614e4a7ea3f124c7
idris <idrisbahce26@gmail.com>
2022-09-01 11:23:46 +0300

    checkpoint4.08

---
+++ src/chapters/chapter04/chapter04checkpoints/CheckPoint4_08.java
@@ -0,0 +1,14 @@
+package chapters.chapter04.chapter04checkpoints;
+
+public class CheckPoint4_08 {
+	public static void main(String[] args) {
+		
+		System.out.println((int)'1');
+		System.out.println((int)'A');
+		System.out.println((int)'B');
+		System.out.println((int)'a');
+		System.out.println((int)'b');
+		
+	}
+
+}
