d6086cefa837553ab875b3bfd365aa976b5f50b4
idris <idrisbahce26@gmail.com>
2022-09-05 14:13:32 +0300

    excercise4_12 revised

--- src/chapters/chapter04/chapter04excercises/Excercise4_12.java
+++ src/chapters/chapter04/chapter04excercises/Excercise4_12.java
@@ -3,25 +3,32 @@ package chapters.chapter04.chapter04excercises;
 import java.util.Scanner;
 
 public class Excercise4_12 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a hex digit:");
-		String hexString = in.next();
-		String hexUpper = hexString.toUpperCase();
-		
-		if(hexString.length()!=1) {
-			System.out.println("You must enter exactly one character");
-			System.exit(1);
-		}
-		char ch = hexUpper.charAt(0);
-		if('A'<=ch && ch<='F') {
-			int value = ch -'A' + 2;
-			System.out.println("The binary value for hex digit " + ch + " is " + value);
-		}else if (Character.isDigit(ch)) {
-			System.out.println("The binary value for hex digit " + ch + " is " + ch);
-		}else if (Character.isDigit(ch)) {
-			System.out.println("The binary value for hex digit " + ch + " is " + ch);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a hex digit: ");
+        String s = in.next();
+        String test = "0123456789ABCDEF";
+        if (test.contains(s)) {
+            s = s.replaceAll("0", "0000");
+            s = s.replaceAll("1", "0001");
+            s = s.replaceAll("2", "0010");
+            s = s.replaceAll("3", "0011");
+            s = s.replaceAll("4", "0100");
+            s = s.replaceAll("5", "0101");
+            s = s.replaceAll("6", "0110");
+            s = s.replaceAll("7", "0111");
+            s = s.replaceAll("8", "1000");
+            s = s.replaceAll("9", "1001");
+            s = s.replaceAll("A", "1010");
+            s = s.replaceAll("B", "1011");
+            s = s.replaceAll("C", "1100");
+            s = s.replaceAll("D", "1101");
+            s = s.replaceAll("E", "1110");
+            s = s.replaceAll("F", "1111");
+            System.out.println("The binary value is " + s);
 
-}
+        } else {
+            System.out.println("Invalid Input.");
+        }
+
+    }}
