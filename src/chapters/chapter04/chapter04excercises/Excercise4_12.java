1e0a191423bcef77e626937c4d3981ea2237f8ee
idris <idrisbahce26@gmail.com>
2022-09-05 18:25:33 +0300

    revised excercise4_12

--- src/chapters/chapter04/chapter04excercises/Excercise4_12.java
+++ src/chapters/chapter04/chapter04excercises/Excercise4_12.java
@@ -3,32 +3,48 @@ package chapters.chapter04.chapter04excercises;
 import java.util.Scanner;
 
 public class Excercise4_12 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a hex digit: ");
-        String s = in.next();
-        String test = "0123456789ABCDEF";
-        if (test.contains(s)) {
-            s = s.replaceAll("0", "0000");
-            s = s.replaceAll("1", "0001");
-            s = s.replaceAll("2", "0010");
-            s = s.replaceAll("3", "0011");
-            s = s.replaceAll("4", "0100");
-            s = s.replaceAll("5", "0101");
-            s = s.replaceAll("6", "0110");
-            s = s.replaceAll("7", "0111");
-            s = s.replaceAll("8", "1000");
-            s = s.replaceAll("9", "1001");
-            s = s.replaceAll("A", "1010");
-            s = s.replaceAll("B", "1011");
-            s = s.replaceAll("C", "1100");
-            s = s.replaceAll("D", "1101");
-            s = s.replaceAll("E", "1110");
-            s = s.replaceAll("F", "1111");
-            System.out.println("The binary value is " + s);
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a hex digit: ");
+		String s = in.next();
+		String str = s.toUpperCase();
+		char ch = str.charAt(0);
 
-        } else {
-            System.out.println("Invalid Input.");
-        }
+		if (ch == 0) {
+			System.out.println("The binary value is 0000");
+		} else if (ch == 1) {
+			System.out.println("The binary value is 0001");
+		} else if (ch == 2) {
+			System.out.println("The binary value is 0010");
+		} else if (ch == 3) {
+			System.out.println("The binary value is 0011");
+		} else if (ch == 4) {
+			System.out.println("The binary value is 0100");
+		} else if (ch == 5) {
+			System.out.println("The binary value is 0101");
+		} else if (ch == 6) {
+			System.out.println("The binary value is 0110");
+		} else if (ch == 7) {
+			System.out.println("The binary value is 0111");
+		} else if (ch == 8) {
+			System.out.println("The binary value is 1000");
+		} else if (ch == 9) {
+			System.out.println("The binary value is 1001");
+		} else if (ch == 'A') {
+			System.out.println("The binary value is 1010");
+		} else if (ch == 'B') {
+			System.out.println("The binary value is 1011");
+		} else if (ch == 'C') {
+			System.out.println("The binary value is 1100");
+		} else if (ch == 'D') {
+			System.out.println("The binary value is 1101");
+		} else if (ch == 'E') {
+			System.out.println("The binary value is 1110");
+		} else if (ch == 'F') {
+			System.out.println("The binary value is 1111");
+		} else {
+			System.out.println("Invalid Input.");
 
-    }}
+		}
+	}
+}
