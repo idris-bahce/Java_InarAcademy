466b552673580cb77b7d584960342c6e66924e53
idris <idrisbahce26@gmail.com>
2022-09-07 13:18:17 +0300

    chapter5

---
+++ src/chapters/chapter05/chapter05listings/FirstLoop.java
@@ -0,0 +1,11 @@
+package chapters.chapter05.chapter05listings;
+
+public class FirstLoop {
+	public static void main(String[] args) {
+		int i;
+		for(i=1; i<=10; i++) {
+			System.out.println("Hello Java!");
+		}
+	}
+
+}
