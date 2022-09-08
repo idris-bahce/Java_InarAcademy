c9bea506b114da350d36b2013157f6b374ea282a
idris <idrisbahce26@gmail.com>
2022-09-08 07:16:55 +0300

    Correction:listing5.1

--- src/chapters/chapter05/chapter05listings/RepeadAdditionQuiz.java
+++ src/chapters/chapter05/chapter05listings/RepeadAdditionQuiz.java
@@ -9,13 +9,19 @@ public class RepeadAdditionQuiz {
 		Scanner in = new Scanner(System.in);
 		System.out.println("What is the result of this equation: "+num1+ " + " + num2);
 		int answer = in.nextInt();
-		
-		while(answer!=num1+num2) {
+
+		int repeat = 0;
+		while(answer!=num1+num2 && repeat<2) {
+			repeat++;
 			System.out.println("Wrong! Try again...");
 			System.out.println("What is the result of this equation: "+num1+ " + " + num2);
 			answer=in.nextInt();
 		}
+		if(answer == num1 + num2) {
 		System.out.println("True!");
+		}else {
+			System.out.println("Enough! Dont play with me anymore!!");
+		}
 	}
 
 }
