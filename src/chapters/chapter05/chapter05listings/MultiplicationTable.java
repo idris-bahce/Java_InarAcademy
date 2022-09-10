93e1920f34ae226eee63b005ef820f7a983fcce5
idris <idrisbahce26@gmail.com>
2022-09-10 16:33:20 +0300

    repaired code

--- src/chapters/chapter05/chapter05listings/MultiplicationTable.java
+++ src/chapters/chapter05/chapter05listings/MultiplicationTable.java
@@ -3,14 +3,14 @@ package chapters.chapter05.chapter05listings;
 public class MultiplicationTable {
 	public static void main(String[] args) {
 		System.out.println("\t\t\t\tMULTIPLICATION TABLE");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 1; i < 10; i++) {
 			System.out.print("\t" + i + "");
 		}
 		System.out.println("\n-------------------------------------------------------------------------------------");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 1; i < 10; i++) {
 			System.out.print(i + " |");
-			for (int j = 0; j < 10; j++) {
-				System.out.print("\t" + j * i);
+			for (int j = 1; j < 10; j++) {
+				System.out.printf("\t%2d", i * j);
 			}
 			System.out.println();
 		}
