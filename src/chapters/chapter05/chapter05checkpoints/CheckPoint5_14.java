a3a6ce1c30b56dcda590d77895f622e60e0f958a
idris <idrisbahce26@gmail.com>
2022-09-12 07:47:25 +0300

    checkpoin5_10

--- src/chapters/chapter05/chapter05checkpoints/CheckPoint5_14.java
+++ src/chapters/chapter05/chapter05checkpoints/CheckPoint5_14.java
@@ -1,9 +1,5 @@
 package chapters.chapter05.chapter05checkpoints;
 
 public class CheckPoint5_14 {
-	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++) {
-			System.out.println(i);
-		}
-	}
+	
 }
