73e0ff21b5905a387fe74c9531351d3d5eea8295
idris <idrisbahce26@gmail.com>
2022-09-13 09:21:59 +0300

    repairment of excercise5_01

--- src/chapters/chapter05/chapter05excercises/Excercise5_01.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_01.java
@@ -9,20 +9,28 @@ public class Excercise5_01 {
 		int i = in.nextInt();
 		int positiveCount = 0;
 		int negativeCount = 0;
-		int total = 0;
-		while (i != 0){
+		double total = 0;
 
-			if (i > 0) {
-				positiveCount++;
-			} else {
-				negativeCount++;
+		if (i != 0) {
+			while (i != 0) {
+
+				if (i > 0) {
+					positiveCount++;
+				} else {
+					negativeCount++;
+				}
+				total = total + i;
+				i = in.nextInt();
 			}
-			 total = total + i;
-			i = in.nextInt();
+
+			double average = total / (positiveCount + negativeCount);
+			System.out.println("The number of positives is " + positiveCount);
+			System.out.println("The number of negatives is " + negativeCount);
+			System.out.println("The total " + total);
+			System.out.println("The avarage is " + average);
+		} else {
+			System.out.println("No numbers are entered except 0");
 		}
-		System.out.println("The number of positives is " + positiveCount);
-		System.out.println("The number of negatives is " + negativeCount);
-		System.out.println("The total " + total);
-		System.out.println("The avarage is " +(total / (positiveCount+negativeCount)));
 	}
+
 }
--- src/chapters/chapter05/chapter05excercises/Excercise5_10.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_10.java
@@ -1,5 +1,8 @@
 package chapters.chapter05.chapter05excercises;
 
 public class Excercise5_10 {
+	public static void main(String[] args) {
+		
+	}
 
 }
