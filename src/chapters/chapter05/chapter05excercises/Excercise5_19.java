c1926a1c4a63079da06a6eb93c09bf60e703c558
idris <idrisbahce26@gmail.com>
2022-09-15 10:22:46 +0300

    unfinished exc.5_19

--- src/chapters/chapter05/chapter05excercises/Excercise5_19.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_19.java
@@ -4,24 +4,25 @@ package chapters.chapter05.chapter05excercises;
 public class Excercise5_19 {
 	public static void main(String[] args) {
 		int i = 8;
-		int k = 1;
+		int k = 64;
+		int z = k - 1;
 		int l = 2;
 		while (i > 0) {
 
 			for (int j = i; j > 0; j--) {
 				System.out.print("    ");
 			}
-			for (int j = 1; j < l + 1 ; j*=2) {
-				System.out.print(j + "   ");
+			for (int j = 1; j < l  ; j*=2) {
+				System.out.printf("%4d",j);
 			}
-		//	for (int j2 = 2; j2 <= k; j2*=j2) {
-		//		System.out.print(j2 + " ");
-		//	}
+			for (int j2 = k; j2 > 0; j2*=2) {
+				System.out.printf("%4d",j2);
+			}
 
 			System.out.println();
 			i--;
 			l*=2;
-			k*=2;
+			k/=2;
 		}
 	
 	}
