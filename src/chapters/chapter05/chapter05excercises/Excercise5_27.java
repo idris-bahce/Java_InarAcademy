1636c320cf0923f2c631c6b829f442fc5e9d213b
idris <idrisbahce26@gmail.com>
2022-09-16 06:54:09 +0300

    exc5_27

--- src/chapters/chapter05/chapter05excercises/Excercise5_27.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_27.java
@@ -3,9 +3,9 @@ package chapters.chapter05.chapter05excercises;
 public class Excercise5_27 {
 	public static void main(String[] args) {
 		int counter = 0;
-		for (int i = 101; i < 2100; i++) {
-			if (i % 4 == 0) {
-				System.out.printf("%5d",i);
+		for (int year = 101; year < 2100; year++) {
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+				System.out.printf("%5d",year);
 				counter++;
 
 				if (counter % 10 == 0)
