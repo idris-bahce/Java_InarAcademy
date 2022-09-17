3bb24128b4cfd3d07367fcb495e7b704c1d3c6ee
idris <idrisbahce26@gmail.com>
2022-09-17 09:50:15 +0300

    exc.

--- src/chapters/chapter05/chapter05excercises/Excercise5_45.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_45.java
@@ -1,5 +1,24 @@
 package chapters.chapter05.chapter05excercises;
 
-public class Excercise5_45 {
+import java.util.Scanner;
 
+public class Excercise5_45 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter ten intiger: ");
+		double sum = 0;
+		double mean = 0;
+		double step1toDeviation = 0;
+		double standartDeviation = 0;
+		for (int i = 0; i < 10; i++) {
+			double num = in.nextDouble();
+			sum += num;
+			mean = sum /10;
+			step1toDeviation +=Math.pow(mean - num, 2);
+		}
+		
+		standartDeviation = Math.sqrt(step1toDeviation);
+		System.out.println("The mean is: " + mean);
+		System.out.println("The standart deviation is: " + standartDeviation);
+	}
 }
--- src/chapters/chapter05/chapter05excercises/Excercise5_46.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_46.java
@@ -1,5 +1,24 @@
 package chapters.chapter05.chapter05excercises;
 
-public class Excercise5_46 {
+import java.util.Scanner;
 
+public class Excercise5_46 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		double sum = 0;
+		double sumSquare = 0;
+		System.out.println("Enter ten numbers: ");
+		for (int i = 1; i <= 10; i++) {
+			double num = input.nextDouble();
+			sum += num;
+			sumSquare = num * num;
+		}
+		double mean = sum / 10;
+		System.out.println("The mean is " + mean);
+
+		double deviation = Math.sqrt((sumSquare - ((sum * sum) / 10)) / 9);
+		System.out.println("The standard deviation is " + deviation);
+
+	}
 }
