95e8f886fdfc73f1dd15670017e1b837ef865406
idris <idrisbahce26@gmail.com>
2022-09-18 06:40:28 +0300

    exc.

--- src/chapters/chapter05/chapter05excercises/Excercise5_48.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_48.java
@@ -5,27 +5,16 @@ import java.util.Scanner;
 public class Excercise5_48 {
 	 public static void main(String[] args) {
 	        Scanner in = new Scanner(System.in);
-
-	        System.out.print("Enter the first 12 digits of an ISBN-13 as a string: ");
-	        String isbn = in.nextLine();
-
-	        // Verify string length
-	        if (isbn.length() != 12) {
-	            System.out.println("Invalid input " + isbn + " input MUST be exactly 12 numbers");
-	            System.exit(0);
-	        }
-
-	        int check = 0;
-	        for (int i = 0; i < isbn.length(); i++) {
-	            int value = Integer.parseInt(String.valueOf(isbn.charAt(i)));
-	            check += i % 2 == 0 ? 3 * value
-	                    : value;
-	        }
-	        check = 10 - check % 10;
-	        
-	        System.out.println("ISBN-13 number is " + isbn +
-	                (check == 10 ? 0 : check));
-
-	        in.close();
+	        System.out.println("Enter a string:");
+	        String str = in.nextLine();
+	        str=str.trim();
+	        int letters = str.length();
+	        String oddStr = "";
+	        for (int i = 0; i < letters; i++) {
+				char ch = str.charAt((int)(Math.random() * letters));
+				oddStr = oddStr + ch;
+			}
+	        System.out.println(oddStr);
+	      
 	    }
 }
