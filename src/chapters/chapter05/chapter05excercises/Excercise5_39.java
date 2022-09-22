270d2fc60a1c79cd73cf57163b7b85ccacb40e6a
idris <idrisbahce26@gmail.com>
2022-09-22 06:51:22 +0300

    exc.

--- src/chapters/chapter05/chapter05excercises/Excercise5_39.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_39.java
@@ -4,11 +4,11 @@ public class Excercise5_39 {
 	public static void main(String[] args) {
 		double sum = 30000;
 		for (int amount = 1; amount < 2000000; amount++) {
-			if(sum < 5000 * 0.08 + 5000 * 0.1 + amount * 0.12) {
+			if (sum < 5000 * 0.08 + 5000 * 0.1 + amount * 0.12) {
 				System.out.println("The amount salesmen has to sell for 30K is: " + (amount + 10000));
 				break;
 			}
-			
 		}
 	}
+	
 }
