be72684b630acb7c6f35d649fa9e80cf3cd7e431
idris <idrisbahce26@gmail.com>
2022-09-24 13:25:40 +0300

    exc 44

--- src/chapters/chapter05/chapter05excercises/Excercise5_44.java
+++ src/chapters/chapter05/chapter05excercises/Excercise5_44.java
@@ -4,25 +4,19 @@ import java.util.Scanner;
 
 public class Excercise5_44 {
 	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
 
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter an integer to convert binary: ");
-		int decimal = in.nextInt();
-		int binary = 0;
-		String sbinary = "";
+	        System.out.print("Enter a short integer: ");
+	        short number = in.nextShort();
 
-		while (decimal > 0) {
-			binary = decimal % 2;
-			sbinary = binary + sbinary;
-			decimal /= 2;
-		}
-		int chstr = sbinary.length();
-		String str0 = "0";
-		while (chstr < 16) {
-			sbinary = str0 + sbinary;
-			chstr++;
-		}
-		System.out.println(sbinary);
+	        String bits = "";
+
+	        for (int i = 0; i < 16; i++) {
+	            bits = (number & 1) + bits;
+	            number >>= 1;
+	        }
+	        in.close();
+	        System.out.println("The bits are " + bits);
 
 	}
 }
